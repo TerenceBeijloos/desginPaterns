c14c77a2480c129aca1ccd529f81acaee0fae3a8
TerenceBeijloos <tgmbeijl@avans.nl>
2020-05-21 11:02:44 +0200

    test commit

--- depa/AND.h
+++ depa/AND.h
@@ -1,2 +1,39 @@
 #pragma once
 
+#define INPUTS 10;
+
+class AND :: public Node {
+public:
+	AND();
+	virtual ~AND();
+	bool compareValues(bool inputValues[]);
+
+private:
+	bool inputValues[INPUTS];
+	bool result;
+};
+
+
+#ifndef __Afgeleide
+#define __Afgeleide
+
+#include "Basis.h"
+
+class Afgeleide : public  Basis
+{
+private:
+                        Afgeleide();
+                        Afgeleide( int );
+public:
+    virtual            ~Afgeleide();
+
+public:
+            void        actie()         override;
+
+            Basis      *clone() const   override;
+
+private:
+    static Afgeleide    m_cInstance;
+};
+
+#endif
--- depa/main.cpp
+++ depa/main.cpp
@@ -1,3 +1,21 @@
+#include "FactoryMethod.h"
+#include "Basis.h"
+
 #include <iostream>
 
-int main
+int main()
+{
+    Basis  *pBasis  = Factory::FactoryMethod<int,Basis>::create( 1 );
+
+    if ( pBasis != nullptr )
+    {
+        pBasis->actie();
+
+        delete pBasis;
+    }
+
+    std::cin.get();
+
+    return 0;
+}
+
